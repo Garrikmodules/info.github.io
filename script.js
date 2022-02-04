905b21c708c01be1a26b6473107fc2494dddaf2d
Garri <89676306+Garrikmodules@users.noreply.github.com>
2022-02-04 14:41:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+/*const fullSize = document.querySelector(".full-size");
+const background = document.querySelector(".bg");
+
+Draggable.create(background);
+
+document.addEventListener("click", () => {
+	const state = Flip.getState(".bg, .full-size");
+  console.log(state);
+
+	fullSize.classList.toggle("active");
+  background.classList.toggle("active");
+
+	Flip.from(state, {
+		duration: 0.6,
+		fade: true,
+		absolute: true,
+		toggleClass: "flipping",
+		ease: "power2.inOut"
+	});
+  
+});
+*/
+
+const fullSize = document.querySelector(".full-size");
+const background = document.querySelector(".bg");
+
+const dur = 0.4;
+gsap.utils.toArray('.item img').forEach(img => {
+  img.addEventListener('click', () => {
+    
+    gsap.set(img.parentNode, { height: gsap.getProperty(img.parentNode, "height") });
+
+			const state = Flip.getState(img),
+				    isOpen = img.classList.toggle('opened');
+
+			gsap.set(img, { xPercent: isOpen ? -80 : 0 });
+
+			Flip.from(state, {
+        duration: 0.6,
+        fade: true,
+        absolute: true,
+        toggleClass: "flipping",
+        ease: "power2.inOut"
+      });
+  });
+});
